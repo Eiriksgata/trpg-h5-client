3db0973c9fcb5f5749a0779ef53b82b6bfd0b57b
Keith404 <2353686862@qq.com>
2020-08-13 15:25:21 +0800

    点击图片等数据放大功能实现

--- resource/js/chatroom/plug/photos.js
+++ resource/js/chatroom/plug/photos.js
@@ -1,7 +1,22 @@
-
-
-function photoZoom(control) {
-
-
-
-}
+(function () {
+    $("body").on("click", "message>img", function () {
+        let layerWidth = ($(window).width() * 0.9);
+        let imgWidth = parseInt($(this).css("width"));
+        let multiple = layerWidth / imgWidth;
+        let imgHeight = parseInt($(this).css("height"));
+        let imgSrc = $(this).attr("src");
+        //计算页面框显示的倍数
+        let layerHeight = multiple * imgHeight;
+        if (layerHeight > $(window).height()) {
+            layerHeight = $(window).width();
+        }
+        layer.open({
+            type: 1,
+            title: false,
+            closeBtn: 0, //不显示关闭按钮
+            shadeClose: true, //开启遮罩关闭
+            area: [layerWidth + 'px', layerHeight + "px"],
+            content: '<img style="width: 100%" src="' + imgSrc + '">'
+        });
+    });
+})();
